b6abd9d4b5b6440a1c906ab570f44f6c7086283f
l1m2e <1956227985@qq.com>
2025-03-12 23:17:00 +0800

    feat: 更新字体大小选择功能，支持直接使用pt单位

--- src/components/Editor/components/EditTopMenu/FontSize.vue
+++ src/components/Editor/components/EditTopMenu/FontSize.vue
@@ -4,22 +4,20 @@ import { SelectTrigger } from 'reka-ui'
 
 const { editor } = defineProps<{ editor: Editor }>()
 
-const fontSize = ref(12)
-const fontSizeList = [8, 9, 10, 11, 12, 14, 16, 18, 20, 24]
+const fontSizeList = ['8pt', '9pt', '10pt', '11pt', '12pt', '14pt', '16pt', '18pt', '20pt', '24pt']
 
-function onSetFontSize(size: number) {
-  editor.chain().focus().setFontSize(`${size}pt`).run()
-  fontSize.value = 12
+function onSetFontSize(size: string) {
+  editor.chain().focus().setFontSize(size).run()
 }
 
 const computedFontSize = computed(() => {
-  const activeFontSize = fontSizeList.find(item => editor.isActive('textStyle', { fontSize: `${item}pt` }))
-  return activeFontSize ? `${activeFontSize}pt` : '12pt'
+  const activeFontSize = fontSizeList.find(item => editor.isActive('textStyle', { fontSize: item }))
+  return activeFontSize || '12pt'
 })
 </script>
 
 <template>
-  <Select v-model="fontSize">
+  <Select>
     <SelectTrigger as-child class="w-12">
       <Button variant="ghost" class="outline-none">
         {{ computedFontSize }}
@@ -28,7 +26,7 @@ const computedFontSize = computed(() => {
 
     <SelectContent>
       <SelectItem v-for="item in fontSizeList" :key="item" :value="item" @select="() => onSetFontSize(item)">
-        {{ item }}pt
+        {{ item }}
       </SelectItem>
     </SelectContent>
   </Select>
