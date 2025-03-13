e95249f8d7f4f52c98b7727d38a94f267f40e74f
l1m2e <1956227985@qq.com>
2025-03-13 20:38:00 +0800

    feat 添加table 命令

--- src/components/Editor/extensions/index.ts
+++ src/components/Editor/extensions/index.ts
@@ -1,4 +1,8 @@
 import Placeholder from '@tiptap/extension-placeholder'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
 import TextStyle from '@tiptap/extension-text-style'
 import StarterKit from '@tiptap/starter-kit'
 import Commands from './Commands'
@@ -17,4 +21,11 @@ export default [
   TextStyle.configure({ mergeNestedSpanStyles: true }),
   Commands.configure({ suggestion }),
   Placeholder.configure({ placeholder: '输入 / 可打开操作面板 ...' }),
+
+  TableRow,
+  TableHeader,
+  TableCell,
+  Table.configure({
+    resizable: true,
+  }),
 ]
