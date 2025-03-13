4aca457767f0b675f32a3b130bc0746bfb4f7144
l1m2e <1956227985@qq.com>
2025-03-13 14:30:25 +0800

    feat 编辑器更新自动设置焦点和预览使用pointer-events-none 实现

--- src/components/Editor/Editor.vue
+++ src/components/Editor/Editor.vue
@@ -17,11 +17,12 @@ const editor = useEditor({
   content: content.value,
   extensions,
   onUpdate: () => content.value = editor.value?.getHTML() || '',
-  editable: mode === 'designer',
 })
 
 watch(content, (val) => {
-  editor.value?.commands.setContent(val, false, { preserveWhitespace: 'full' })
+  const { from, to } = editor.value!.state.selection
+  editor.value?.commands.setContent(val, false, { preserveWhitespace: true })
+  editor.value?.commands.setTextSelection({ from, to })
 })
 
 const SelectFieldDialogRef = useTemplateRef('SelectFieldDialogEl')
@@ -36,6 +37,7 @@ provide(EDITOR_CONTEXT, {
   openSelectFieldDialog,
 })
 
+const editorContentClass = computed(() => mode === 'designer' ? 'p-4' : 'pointer-events-none')
 defineExpose({
   editor,
 })
@@ -44,7 +46,7 @@ defineExpose({
 <template>
   <div v-if="editor" class="tiptap-editor" text-black dark:border-neutral-800 dark:bg-black dark:text-white>
     <EditTopMenu v-if="mode === 'designer'" :editor />
-    <EditorContent :editor :class="mode === 'designer' && 'p-4'" />
+    <EditorContent :editor :class="editorContentClass" />
     <SelectFieldDialog ref="SelectFieldDialogEl" />
   </div>
 </template>
