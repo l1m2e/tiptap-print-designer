b8563f80b76d80674c3b739bc7607d13bf9c336a
l1m2e <1956227985@qq.com>
2025-03-16 04:47:29 +0800

    feat 添加表格操作功能

--- src/components/Designer/components/EditTopMenu/FontSize.vue
+++ src/components/Designer/components/EditTopMenu/FontSize.vue
@@ -1,17 +1,17 @@
 <script lang="ts" setup>
-import type { Editor } from '@tiptap/vue-3'
 import { SelectTrigger } from 'reka-ui'
+import { EDITOR_CONTEXT } from '~/components/Editor/constants'
 
-const { editor } = defineProps<{ editor: Editor }>()
+const { editor } = inject(EDITOR_CONTEXT)!
 
 const fontSizeList = ['8pt', '9pt', '10pt', '11pt', '12pt', '14pt', '16pt', '18pt', '20pt', '24pt']
 
 function onSetFontSize(size: string) {
-  editor.chain().focus().setFontSize(size).run()
+  editor.value?.chain().focus().setFontSize(size).run()
 }
 
 const computedFontSize = computed(() => {
-  const activeFontSize = fontSizeList.find(item => editor.isActive('textStyle', { fontSize: item }))
+  const activeFontSize = fontSizeList.find(item => editor.value?.isActive('textStyle', { fontSize: item }))
   return activeFontSize || '12pt'
 })
 </script>
