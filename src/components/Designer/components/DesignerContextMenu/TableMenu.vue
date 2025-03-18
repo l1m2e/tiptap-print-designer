e3490ec8b6700de5e4662e22946f3f3d00777f7f
l1m2e <1956227985@qq.com>
2025-03-18 18:00:13 +0800

    feat 命令调用直接绑定到按钮上

--- src/components/Designer/components/DesignerContextMenu/TableMenu.vue
+++ src/components/Designer/components/DesignerContextMenu/TableMenu.vue
@@ -2,48 +2,16 @@
 import { EDITOR_CONTEXT } from '~/components/Editor/constants'
 
 const { editor } = inject(EDITOR_CONTEXT)!
-
-function deleteTable() {
-  editor.value?.chain().focus().deleteTable().run()
-}
-
-function addRowAfter() {
-  editor.value?.chain().focus().addRowAfter().run()
-}
-
-function addRowBefore() {
-  editor.value?.chain().focus().addRowBefore().run()
-}
-
-function addColumnBefore() {
-  editor.value?.chain().focus().addColumnBefore().run()
-}
-
-function addColumnAfter() {
-  editor.value?.chain().focus().addColumnAfter().run()
-}
-
-function mergeOrSplit() {
-  editor.value?.chain().focus().mergeOrSplit().run()
-}
-
-function deleteRow() {
-  editor.value?.chain().focus().deleteRow().run()
-}
-
-function deleteColumn() {
-  editor.value?.chain().focus().deleteColumn().run()
-}
 </script>
 
 <template>
   <ContextMenuSub>
     <ContextMenuSubTrigger>添加行</ContextMenuSubTrigger>
     <ContextMenuSubContent>
-      <ContextMenuItem @click="addRowAfter">
+      <ContextMenuItem @click="editor?.chain().focus().addRowAfter().run()">
         下方添加一行
       </ContextMenuItem>
-      <ContextMenuItem @click="addRowBefore">
+      <ContextMenuItem @click="editor?.chain().focus().addRowBefore().run()">
         上方添加一行
       </ContextMenuItem>
     </ContextMenuSubContent>
@@ -52,32 +20,32 @@ function deleteColumn() {
   <ContextMenuSub>
     <ContextMenuSubTrigger>添加列</ContextMenuSubTrigger>
     <ContextMenuSubContent>
-      <ContextMenuItem @click="addColumnBefore">
+      <ContextMenuItem @click="editor?.chain().focus().addColumnBefore().run()">
         左方添加一列
       </ContextMenuItem>
-      <ContextMenuItem @click="addColumnAfter">
+      <ContextMenuItem @click="editor?.chain().focus().addColumnAfter().run()">
         右方添加一列
       </ContextMenuItem>
     </ContextMenuSubContent>
   </ContextMenuSub>
 
-  <ContextMenuItem @click="mergeOrSplit">
+  <ContextMenuItem @click="editor?.chain().focus().mergeCells().run()">
     合并单元格
   </ContextMenuItem>
-  <ContextMenuItem @click="mergeOrSplit">
+  <ContextMenuItem @click="editor?.chain().focus().splitCell().run()">
     拆分单元格
   </ContextMenuItem>
 
   <ContextMenuSub>
     <ContextMenuSubTrigger>删除</ContextMenuSubTrigger>
     <ContextMenuSubContent>
-      <ContextMenuItem @click="deleteRow">
+      <ContextMenuItem @click="editor?.chain().focus().deleteRow().run()">
         删除当前行
       </ContextMenuItem>
-      <ContextMenuItem @click="deleteColumn">
+      <ContextMenuItem @click="editor?.chain().focus().deleteColumn().run()">
         删除当前列
       </ContextMenuItem>
-      <ContextMenuItem @click="deleteTable">
+      <ContextMenuItem @click="editor?.chain().focus().deleteTable().run()">
         删除表格
       </ContextMenuItem>
     </ContextMenuSubContent>
