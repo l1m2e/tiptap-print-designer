9e7902f1d6edf0769cd215297199eb39a76c5a32
l1m2e <1956227985@qq.com>
2025-03-21 22:40:00 +0800

    feat 添加ActionButton 组件

---
+++ src/components/common/action-button/ActionButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { ActionButtonProps } from '.'
+
+const { title, icon, fn } = defineProps<ActionButtonProps>()
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child>
+        <Button size="icon" class="font-black" variant="ghost" @click="fn">
+          <component :is="icon" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent> {{ title }} </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
