940747c316e51173f3165cc4858202800639b620
l1m2e <1956227985@qq.com>
2025-03-25 17:37:25 +0800

    feat 数据表格 新增列宽调整功能

--- src/Editor/extensions/DateTableNode/DataTable.vue
+++ src/Editor/extensions/DateTableNode/DataTable.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts" generic="TData, TValue">
-import type { ColumnDef } from '@tanstack/vue-table'
+<script setup lang="tsx" generic="TData, TValue">
+import type { ColumnDef, ColumnSizingState } from '@tanstack/vue-table'
 
 import {
   FlexRender,
@@ -13,27 +13,46 @@ const { columns, data, rowId = 'id' } = defineProps<{
   rowId?: string
 }>()
 
+const emits = defineEmits<{ columnSizingChange: [val: ColumnSizingState] }>()
+
 const tableData = ref<any[]>([])
 
-watchImmediate(() => data, (val: TData[]) => tableData.value = [...val], { deep: true })
+watchImmediate(() => data, (val: TData[]) => val && (tableData.value = [...val]), { deep: true })
 
 const table = useVueTable({
   get data() { return tableData.value },
   get columns() { return columns },
   getRowId: originalRow => originalRow[rowId],
   getCoreRowModel: getCoreRowModel(),
+  columnResizeMode: 'onChange',
+})
+
+watch(() => table.getState().columnSizing, (val: ColumnSizingState) => {
+  emits('columnSizingChange', val)
+})
+
+defineExpose({
+  table,
 })
 </script>
 
 <template>
-  <div class="border rounded-md">
-    <table>
+  <div class="max-w-full overflow-y-hidden">
+    <table class="w-full">
       <thead>
         <tr v-for="headerGroup in table.getHeaderGroups()" :key="headerGroup.id">
-          <th v-for="header in headerGroup.headers" :key="header.id">
+          <th v-for="header in headerGroup.headers" :key="header.id" class="relative group" :style="{ width: `${header.column.getSize()}pt` }">
             <FlexRender
               v-if="!header.isPlaceholder" :render="header.column.columnDef.header"
               :props="header.getContext()"
+            />
+            <div
+              v-if="header.column.getCanResize()"
+              class="absolute h-full right-0 w-1 top-0 opacity-0 group-hover:opacity-100 transition-opacity cursor-col-resize select-none touch-none"
+              :class=" `${header.column.getIsResizing() ? 'bg-green-500 opacity-100' : 'bg-violet-500'}` "
+              @dblclick="() => header.column.resetSize()"
+              @mousedown="(e) => header.getResizeHandler()(e)"
+              @touchstart="(e) => header.getResizeHandler()(e)"
             />
           </th>
         </tr>
@@ -44,7 +63,7 @@ const table = useVueTable({
             v-for="row in table.getRowModel().rows" :key="row.id"
             :data-state="row.getIsSelected() ? 'selected' : undefined"
           >
-            <td v-for="cell in row.getVisibleCells()" :key="cell.id">
+            <td v-for="cell in row.getVisibleCells()" :key="cell.id" :style="{ width: `${cell.column.getSize()}pt` }">
               <FlexRender :render="cell.column.columnDef.cell" :props="cell.getContext()" />
             </td>
           </tr>
@@ -53,7 +72,7 @@ const table = useVueTable({
           <tr>
             <td :colspan="columns.length">
               <slot name="no-data">
-                No Data.
+                暂无数据
               </slot>
             </td>
           </tr>
--- src/Editor/extensions/DateTableNode/DateTableNode.vue
+++ src/Editor/extensions/DateTableNode/DateTableNode.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { ColumnSizingState } from '@tanstack/vue-table'
 import { nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
 import { get } from 'radash'
 import { DESIGNER_KEY } from '~/Designer'
@@ -9,16 +10,26 @@ const props = defineProps(nodeViewProps)
 const { mode, data } = inject(EDITOR_CONTEXT)!
 const { openDateTableDialog } = inject(DESIGNER_KEY)!
 
+const DataTableRef = useTemplateRef('DataTableEl')
 function edit() {
   openDateTableDialog({ path: props.node.attrs.path, columns: props.node.attrs.columns })
 }
 
-const tableList = computed<any[]>(() => get(data.value, props.node.attrs.path))
+const tableList = computed<any[]>(() => get(data.value, props.node.attrs.path) || [])
 const columns = computed(() => JSON.parse(props.node.attrs.columns))
 
 function ddelete() {
   props.deleteNode()
 }
+
+function handleColumnSizingChange(val: ColumnSizingState) {
+  props.updateAttributes({ columnSizingState: JSON.stringify(val) })
+}
+
+watchImmediate(() => props.node.attrs.columnSizingState, async () => {
+  await nextTick()
+  DataTableRef.value?.table.setColumnSizing(JSON.parse(props.node.attrs.columnSizingState))
+})
 </script>
 
 <template>
@@ -26,7 +37,7 @@ function ddelete() {
     <ContextMenu v-if="mode === 'designer'" class="w-full">
       <ContextMenuTrigger>
         <div class="border border-dashed border-violet-500 rounded p-2 w-full">
-          <DataTable :columns="columns" :data="tableList" />
+          <DataTable ref="DataTableEl" :columns="columns" :data="tableList" @column-sizing-change="handleColumnSizingChange" />
         </div>
       </ContextMenuTrigger>
       <ContextMenuContent>
@@ -39,6 +50,6 @@ function ddelete() {
       </ContextMenuContent>
     </ContextMenu>
 
-    <DataTable v-else :columns="columns" :data="tableList" />
+    <DataTable v-else ref="DataTableEl" :columns="columns" :data="tableList " />
   </NodeViewWrapper>
 </template>
