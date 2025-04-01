577c81b92a5d87a10996cb77c34f99f158fe1084
l1m2e <1956227985@qq.com>
2025-04-01 21:21:00 +0800

    feat 实现时间格式化字段功能

---
+++ src/components/common/template/formatTimestamp.vue
@@ -0,0 +1,8 @@
+<script lang="jsx" setup>
+import { formatDate } from '@vueuse/core'
+const props = defineProps(['value'])
+</script>
+
+<template>
+  {{ formatDate(new Date(value), '$format') }}
+</template>
