92cb46477ae2859b5cd760ce03cf5bc684424389
l1m2e <1956227985@qq.com>
2025-04-03 23:35:00 +0800

    feat 调整 eslint 规则

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ export default antfu(
   {
     formatters: true,
     rules: {
+      'antfu/if-newline': 'off',
       '@typescript-eslint/consistent-type-definitions': 'off',
     },
     ignores: ['src/components/common/template/**/*.vue'],
