9a4ef742d9703c53c979b362cc34a7b549c11db8
l1m2e <1956227985@qq.com>
2025-04-08 15:36:35 +0800

    feat 更新格式化弹窗

--- src/Designer/components/FormatDialog/common/Timestamp.vue
+++ src/Designer/components/FormatDialog/common/Timestamp.vue
@@ -1,12 +1,8 @@
-<!-- eslint-disable unused-imports/no-unused-vars -->
 <script lang="ts" setup>
-import type { NodeViewProps } from '@tiptap/vue-3'
-import formatTimestampTemplate from '~/components/common/template/formatTimestamp.vue?raw'
+import type { Format } from './index'
+import { TimestampTemplate } from '~/components/common/template'
 
-const { nodeProps } = defineProps<{ nodeProps: NodeViewProps, show: boolean, nodeMockData: any }>()
-const emit = defineEmits<{ 'update:show': [show: boolean] }>()
-
-const formatTimestampList = [
+const formatList = [
   { label: 'YYYY-MM-DD', value: 'YYYY-MM-DD' },
   { label: 'YYYY-MM-DD HH:mm', value: 'YYYY-MM-DD HH:mm' },
   { label: 'YYYY-MM-DD HH:mm:ss', value: 'YYYY-MM-DD HH:mm:ss' },
@@ -20,23 +16,21 @@ const formatTimestampList = [
 const selectedFormat = ref('YYYY-MM-DD')
 const customFormat = ref('')
 
-function getFormat() {
+function getFormat(): Format {
   const format = selectedFormat.value === 'custom' ? customFormat.value : selectedFormat.value
-  const template = formatTimestampTemplate.replace('$format', format)
-  return { type: 'Timestamp', template, value: { selectedFormat: selectedFormat.value, customFormat: customFormat.value } }
+  const template = TimestampTemplate.replace('$format', format)
+  return { type: 'Timestamp', template, expands: { selectedFormat: selectedFormat.value, customFormat: customFormat.value } }
 }
 
-function setFormat() {
-  const { value } = JSON.parse(nodeProps.node.attrs.format || '{}')
-  selectedFormat.value = value?.selectedFormat || 'YYYY-MM-DD'
-  customFormat.value = value?.customFormat || ''
+function setFormat(data: Format) {
+  selectedFormat.value = data.expands?.selectedFormat || 'YYYY-MM-DD'
+  customFormat.value = data.expands?.customFormat || ''
 }
-nodeProps?.node?.attrs?.format && setFormat()
 
-function onConfirm() {
-  nodeProps.updateAttributes({ format: JSON.stringify(getFormat()) })
-  emit('update:show', false)
-}
+defineExpose({
+  getFormat,
+  setFormat,
+})
 </script>
 
 <template>
@@ -46,7 +40,7 @@ function onConfirm() {
         <SelectValue placeholder="请选择字段" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem v-for="item in formatTimestampList" :key="item.value" :value="item.value">
+        <SelectItem v-for="item in formatList" :key="item.value" :value="item.value">
           {{ item.label }}
         </SelectItem>
       </SelectContent>
@@ -54,12 +48,4 @@ function onConfirm() {
 
     <Input v-if="selectedFormat === 'custom'" v-model="customFormat" class="w-[400px] mt-4" placeholder="请输入自定义格式" />
   </div>
-  <DialogFooter>
-    <Button variant="outline" @click="emit('update:show', false)">
-      取消
-    </Button>
-    <Button @click="onConfirm">
-      确定
-    </Button>
-  </DialogFooter>
 </template>
---
+++ src/components/common/template/index.ts
@@ -0,0 +1,3 @@
+export { default as DefaultTemplate } from '~/components/common/template/DefaultTemplate.vue?raw'
+export { default as FiledNodeTemplate } from '~/components/common/template/FiledNodeTemplate.vue?raw'
+export { default as TimestampTemplate } from '~/components/common/template/TimestampTemplate.vue?raw'
