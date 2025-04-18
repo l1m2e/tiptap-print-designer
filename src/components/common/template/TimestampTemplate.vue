97cdee941b775d5e393bd4c67baf9f4db6ffcb25
l1m2e <1956227985@qq.com>
2025-04-18 16:24:32 +0800

    feat 初始化 pnpm-monorepo

--- src/components/common/template/TimestampTemplate.vue
+++
@@ -1,8 +0,0 @@
-<script lang="jsx" setup>
-import { formatDate } from '@vueuse/core'
-const {value} = defineProps(['value'])
-</script>
-
-<template>
-  {{ formatDate(new Date(value), '$format') }}
-</template>
