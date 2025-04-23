0c8e6ee9c0994d32c6b0c45a39a1d0a1f9def96c
l1m2e <1956227985@qq.com>
2025-04-23 10:18:37 +0800

    docs: 添加文档指南和组件说明，优化主题配置
    新增了`start.md`、`quick-start.md`、`installation.md`、`designer.md`和`render.md`等文档，详细介绍了项目的使用方法和组件功能。同时，优化了`.vitepress/config.ts`中的主题配置，添加了侧边栏导航和社交链接，提升了文档的可读性和用户体验。

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,3 +1,4 @@
+import type { DefaultTheme } from 'vitepress'
 import path from 'node:path'
 import { defineConfig } from 'vitepress'
 
@@ -6,7 +7,20 @@ export default defineConfig({
   title: 'tiptap-print-designer',
   description: 'tiptap-print-designer',
   base: '/tiptap-print-designer/',
-  srcDir: './src',
+  srcDir: 'src',
+  themeConfig: {
+    sidebar: {
+      '/guide/': { base: '/guide/', items: sidebarGuide() },
+    },
+    search: {
+      provider: 'local',
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/l1m2e/tiptap-print-designer' },
+    ],
+    docFooter: { prev: '上一页', next: '下一页' },
+    outlineTitle: '目录',
+  },
   vite: {
     resolve: {
       alias: {
@@ -16,3 +30,25 @@ export default defineConfig({
     },
   },
 })
+
+function sidebarGuide(): DefaultTheme.SidebarItem[] {
+  return [
+    {
+      text: '指南',
+      collapsed: false,
+      items: [
+        { text: '简介', link: 'start' },
+        { text: '安装', link: 'installation' },
+        { text: '快速上手', link: 'quick-start' },
+      ],
+    },
+    {
+      text: '组件',
+      collapsed: false,
+      items: [
+        { text: 'Designer', link: 'designer' },
+        { text: 'Render', link: 'render' },
+      ],
+    },
+  ]
+}
