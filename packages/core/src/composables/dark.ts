3c3b63e940e7405c2fd94dfbfda1e9f9450244fe
l1m2e <1956227985@qq.com>
2025-08-06 20:02:00 +0800

    feat 移除不成熟的夜间模式 以免对上级应用造成影响

--- packages/core/src/composables/dark.ts
+++ packages/core/src/composables/dark.ts
@@ -1,2 +1,9 @@
-export const isDark = useDark()
-export const toggleDark = useToggle(isDark)
+// // 为特定模块提供黑暗模式支持
+// export const isDark = useDark({
+//   selector: '.tiptap-print-designer-theme',
+//   attribute: 'class',
+//   valueDark: 'dark',
+//   valueLight: '',
+// })
+
+// export const toggleDark = useToggle(isDark)
