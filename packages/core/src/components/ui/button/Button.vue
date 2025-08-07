5e826e09844293aff24fdca73706664589a52441
l1m2e <1956227985@qq.com>
2025-08-07 14:12:05 +0800

    feat 给Button 添加 Loading

--- packages/core/src/components/ui/button/Button.vue
+++ packages/core/src/components/ui/button/Button.vue
@@ -1,19 +1,23 @@
 <script setup lang="ts">
-import type { PrimitiveProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import type { ButtonVariants } from "."
-import { Primitive } from "reka-ui"
+import type { PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { ButtonVariants } from '.'
+import { Loader2 } from 'lucide-vue-next'
+import { Primitive } from 'reka-ui'
 import { cn } from '~/lib/utils'
-import { buttonVariants } from "."
+import { buttonVariants } from '.'
 
 interface Props extends PrimitiveProps {
-  variant?: ButtonVariants["variant"]
-  size?: ButtonVariants["size"]
-  class?: HTMLAttributes["class"]
+  variant?: ButtonVariants['variant']
+  size?: ButtonVariants['size']
+  class?: HTMLAttributes['class']
+  loading?: boolean
+  disabled?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  as: "button",
+  as: 'button',
+  asChild: false,
 })
 </script>
 
@@ -22,7 +26,9 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="props.loading || props.disabled"
   >
+    <Loader2 v-if="props.loading" class="tpd-mr-2 tpd-h-4 tpd-w-4 tpd-animate-spin" />
     <slot />
   </Primitive>
 </template>
