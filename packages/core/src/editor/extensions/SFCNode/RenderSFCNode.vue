75d16d1dca733b5c8bdd0ab403d66495ba1cb83a
l1m2e <1956227985@qq.com>
2025-09-09 15:13:04 +0800

    feat SFCNode 支持通过右键菜单在“块级”和“内联”之间切换 支持“自由拖拽定位”（相对 .ProseMirror 绝对定位），能把组件拖到文档中的任意位置

--- packages/core/src/editor/extensions/SFCNode/RenderSFCNode.vue
+++ packages/core/src/editor/extensions/SFCNode/RenderSFCNode.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import { nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
+import { computed } from 'vue'
 
 const props = defineProps(nodeViewProps)
+
+const isInline = computed(() => props.node.type.name === 'sfc-inline')
+const wrapperStyle = computed(() => {
+  if (props.node.attrs.isFloating) {
+    return {
+      position: 'absolute',
+      left: `${props.node.attrs.x || 0}px`,
+      top: `${props.node.attrs.y || 0}px`,
+      zIndex: props.node.attrs.zIndex || 1,
+    } as Record<string, string | number>
+  }
+  return {
+    display: isInline.value ? 'inline-block' : 'block',
+  }
+})
 </script>
 
 <template>
-  <NodeViewWrapper as="div">
+  <NodeViewWrapper :as="isInline ? 'span' : 'div'" :style="wrapperStyle">
     <SfcLoader :text="props.node.attrs.text" />
   </NodeViewWrapper>
 </template>
